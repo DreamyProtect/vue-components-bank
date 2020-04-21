25315e2e6d36a314416212c243e57597a4f5ba0f
Matthieu <chevreux.matthieu@hotmail.com>
2020-04-21 18:49:23 +0200

    :sparkles: added a navbar and a drawer menu when the screen size changes

---
+++ src/components/NavBar.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul class="flex flex justify-end w-full">
+    <li v-for="(navItem, index) in routes"
+      :key="index">
+      <NavigationElement
+        :name="navItem.name"
+        :path="navItem.path"/>
+    </li>
+  </ul>
+</template>
+
+<script>
+import NavigationElement from '@/components/NavigationElement'
+export default {
+  name: 'NavBar',
+  props: ['routes'],
+  components: {
+    NavigationElement
+  }
+}
+</script>
---
+++ src/components/NavigationElement.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="m-1">
+      <router-link :to="path">{{ name }}</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NavigationElement',
+  props: [
+    'name',
+    'path'
+  ]
+}
+</script>
